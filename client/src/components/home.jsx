c226e03310d0d92a37edab86c3eaaae24edd842d
Isabella <idavila2021@gmail.com>
2024-07-23 18:56:26 +0100

    Redeployment test

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Login from './login.jsx'
 
 const Home = () => {
   return (
     <div>
       <h1>Rover&Out home works!</h1>
+      <Login></Login>
     </div>
   );
 };
