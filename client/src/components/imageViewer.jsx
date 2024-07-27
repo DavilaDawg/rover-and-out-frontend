a34074f7be83e0a98f6dd241d846e597e6b977af
Isabella <idavila2021@gmail.com>
2024-07-27 19:29:49 +0100

    refactor: using states for saving annotated

--- client/src/components/imageViewer.jsx
+++ client/src/components/imageViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom"; // UseLocation hook allows you to access the state object that you passed when navigating
 import { Button } from "@/components/ui/button.jsx";
 import * as markerjs2 from "markerjs2";
@@ -9,7 +9,8 @@ const ImageViewer = () => {
   const imgRef = useRef(null); // Ref to hold the image element
 
   const { imageUrl, isBoring, sol, filter } = location.state || {};
-  const [annotatedImageUrl, setAnnotatedImageUrl] = useState(null);
+  const [maState, setMaState] = useState(null); 
+
 
   // Navigation:
   if (!imageUrl) {
@@ -23,21 +24,32 @@ const ImageViewer = () => {
     navigate(path, { state: { sol } });
   }
 
-  // Annotation:
   useEffect(() => {
     if (imgRef.current) {
-      const markerArea = new markerjs2.MarkerArea(imgRef.current);
-      markerArea.addEventListener("render", (event) => {
-        imgRef.current.src = event.dataUrl; // Updates the display of the annotated image directly in the img element
-      });
-      markerArea.show();
-    }
-  }, [imageUrl]);
+      const sourceImage = imgRef.current;
+      const targetRoot = sourceImage.parentElement;
 
-  function handleSave() {
-    setAnnotatedImageUrl(imgRef.current.src)
-    console.log(annotatedImageUrl)
-  }
+      const markerArea = new markerjs2.MarkerArea(sourceImage);
+      markerArea.targetRoot = targetRoot;
+
+      const handleRender = (event) => {
+        sourceImage.src = event.dataUrl; // Update the image display
+        setMaState(event.state); // Save the state of MarkerArea
+      };
+
+      markerArea.addEventListener("render", handleRender);
+      markerArea.show();
+
+      if (maState) {
+        markerArea.restoreState(maState);
+      }
+
+      // Cleanup on unmount
+      return () => {
+        markerArea.removeEventListener("render", handleRender);
+      };
+    }
+  }, [imageUrl, maState]);
 
   return (
     <>
@@ -56,15 +68,8 @@ const ImageViewer = () => {
         ) : (
           <h1 className="text-6xl ml-2"> Sol: {sol} </h1>
         )}
-
-          <Button
-            onClick={handleSave}
-            className="absolute top-4 right-28 bg-gray-800 text-white px-4 py-2 rounded"
-          >
-            Save Image
-          </Button>
-
       </div>
+
       <img
         ref={imgRef}
         src={imageUrl}
