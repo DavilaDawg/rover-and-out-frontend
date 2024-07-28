6381118e9ba0f3744d65d62fd195922ee560df55
Isabella <idavila2021@gmail.com>
2024-07-28 22:32:58 +0100

    fix: change drill button position

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -48,12 +48,12 @@ const Map = () => {
             Bradbury Landing
           </Button>
 
-          <Button
-            className="absolute top-2/3 mt-20 mr-14 bg-gray-800 text-white px-4 py-2 rounded"
-            onClick={toDrill}
-          >
-            Mary Anning & Grocken Drill Sites
-          </Button>
+            <Button
+              className="absolute top-[53%] transform -translate-x-1/2 mt-80 ml-8 bg-gray-800 text-white px-4 py-2 rounded"
+              onClick={toDrill}
+            >
+              Mary Anning & Grocken Drill Sites
+            </Button>
 
           <Button
             className="absolute top-2/3 left-1/4 bg-gray-800 text-white px-4 py-2 rounded"
