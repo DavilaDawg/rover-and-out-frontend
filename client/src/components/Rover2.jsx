42aa6353b702d886f48de8b046f70b722d595383
Isabella <idavila2021@gmail.com>
2024-07-31 04:34:23 +0100

    feat: all cameras done, need some handle click funcs

--- client/src/components/Rover2.jsx
+++ client/src/components/Rover2.jsx
@@ -33,6 +33,18 @@ export const Rover2 = (props) => {
     navigate("/gallery/NAVCAM");
   }
 
+  function handleChem() {
+    console.log("chem")
+  }
+
+  function handleMast() {
+    console.log("mast")
+  }
+
+  function handleDescent() {
+    console.log("descent")
+  }
+
   useEffect(() => {
     document.body.style.cursor = hoveredMesh ? "pointer" : "auto";
   }, [hoveredMesh]);
@@ -77,66 +89,102 @@ export const Rover2 = (props) => {
           handleClick={handleNav}
           handlePointerOver={() => handlePointerOver("Navigation Camera")}
           handlePointerOut={handlePointerOut}
-          hovered={hoveredMesh === "Navigation Camera"}
+          hovered={hoveredMesh === "Navigation Camera"} // Convert hover prop to bool
         />
 
         {/*Left nav cam:*/}
         <SphereComponent
           radius={0.05}
           position={[-1.8, 1.72, -0.28]}
+          handleClick={handleNav}
+          handlePointerOver={() => handlePointerOver("Navigation Camera")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Navigation Camera"}
         ></SphereComponent>
 
         {/*Mars Hand Lens Imager:*/}
         <SphereComponent
           radius={0.08}
           position={[-0.62, 1.66, 0.64]}
+          handleClick={handleProb}
+          handlePointerOver={() => handlePointerOver("Mars Hand Lens Imager")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Mars Hand Lens Imager"} 
         ></SphereComponent>
 
         {/*Right rear hazcam:*/}
         <SphereComponent
-          radius={0.09}
+          radius={0.11}
           position={[-0.56, 0.55, -2.1]}
+          handleClick={handleRear}
+          handlePointerOver={() => handlePointerOver("Rover Hazard Avoidance Camera")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Rover Hazard Avoidance Camera"} 
         ></SphereComponent>
 
         {/*Left rear hazcam:*/}
         <SphereComponent
-          radius={0.09}
+          radius={0.11}
           position={[-1.56, 0.55, -2.1]}
+          handleClick={handleRear}
+          handlePointerOver={() => handlePointerOver("Rover Hazard Avoidance Camera")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Rover Hazard Avoidance Camera"} 
         ></SphereComponent>
 
         {/*Front left hazcams:*/}
         <SphereComponent
           radius={0.18}
           position={[-1.02, 0.5, -0.28]}
+          handleClick={handleRear}
+          handlePointerOver={() => handlePointerOver("Rover Hazard Avoidance Camera")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Rover Hazard Avoidance Camera"} 
         ></SphereComponent>
 
         {/*ChemCam:*/}
         <SphereComponent
           radius={0.08}
           position={[-1.47, 1.88, -0.28]}
+          handleClick={handleChem}
+          handlePointerOver={() => handlePointerOver("ChemCam")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "ChemCam"} 
         ></SphereComponent>
 
         {/*Mast right:*/}
         <SphereComponent
           radius={0.05}
           position={[-1.46, 1.72, -0.28]}
+          handleClick={handleMast}
+          handlePointerOver={() => handlePointerOver("Mastcam")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Mastcam"} 
         ></SphereComponent>
 
         {/*Mast left:*/}
         <SphereComponent
           radius={0.05}
           position={[-1.7, 1.72, -0.28]}
+          handleClick={handleMast}
+          handlePointerOver={() => handlePointerOver("Mastcam")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Mastcam"} 
         ></SphereComponent>
 
         {/*Mars Descent Imager:*/}
         <SphereComponent
           radius={0.07}
           position={[-0.42, 0.43, -0.42]}
+          handleClick={handleDescent}
+          handlePointerOver={() => handlePointerOver("Mars Descent Imager")}
+          handlePointerOut={handlePointerOut}
+          hovered={hoveredMesh === "Mars Descent Imager"} 
         ></SphereComponent>
 
         {hoveredMesh && (
           <Tooltip
-            position={[-1, 2.5, -1]} // Adjust the position as needed
+            position={[-1, 2.5, -1]} 
             text={hoveredMesh}
           />
         )}
